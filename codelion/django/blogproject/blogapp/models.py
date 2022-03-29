676ed7eb65a6f0a1c60b6093ab730426e96e72a4
soeunkk <luckyber1@naver.com>
2022-03-29 15:02:37 +0900

    [feat] 객체 정보를 title로 출력 (#16)

--- codelion/django/blogproject/blogapp/models.py
+++ codelion/django/blogproject/blogapp/models.py
@@ -5,3 +5,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
